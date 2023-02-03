afdb00296838a59ac9c7e6eb164e329fcae2b246
RiddhiPatel25 <122702771+RiddhiPatel25@users.noreply.github.com>
2023-02-02 19:32:18 -0500

    done

---
+++ public/error/error.js
@@ -0,0 +1,5 @@
+
+//got the idea from these other pens around here.
+
+// https://codepen.io/codetrail/pen/uHvpl
+// https://codepen.io/kaypooma/pen/ehfjC
---
+++ server.js
@@ -0,0 +1,81 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 2
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Riddhi Patel Student ID: 151212214 Date: 02 February 2022
+ *
+ *  Cyclic Web App URL: ________________________________________________________
+ *
+ *  GitHub Repository URL: ______________________________________________________
+ *
+********************************************************************************/
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+var express = require("express");
+var app = express();
+
+const path = require("path");
+const blogService = require("./blog-service.js");
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.get("/", (req, res) => {
+  res.redirect("/about");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/blog", (req, res) => {
+  blogService
+    .getPublishedPosts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
+
+app.get("/posts", (req, res) => {
+  blogService
+    .getAllPosts()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
+
+app.get("/categories", (req, res) => {
+  blogService
+    .getCategories()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(function (err) {
+      console.log("Unable to open the file: " + err);
+    });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile(__dirname + "/views/404/error.html");
+});
+
+blogService
+  .initialize()
+  .then(function () {
+    app.listen(HTTP_PORT, onHttpStart);
+  })
+  .catch(function (err) {
+    console.log("Unable to open the file: " + err);
+  });
